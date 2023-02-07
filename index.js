81e80142d91bb56916d2958b908ed96b2da8bbf4
upoma <tahnumaafrin443@gmail.com>
2023-02-07 12:41:12 +0600

    3 problem done

---
+++ index.js
@@ -0,0 +1,138 @@
+// ------------------FIRST PROBLEM----------------------
+
+// Details of the function-----
+
+// Function name :   mindGame
+// (1) Take a number as input
+// (2) Multiply this number with 3
+// (3) Add 10 with this above result
+// (4) divide by 2 to the above result
+// (5) from the above number subtract 5
+// (2) return the final output  
+
+
+
+// I'm writing this function by using some mathematical operations to get a final output. As well as I follow the given instructions by the instructor.
+
+// function mindGame(){
+//     let inputNumber = 18;
+//     let numMultiplication = (inputNumber * 3);
+//     // console.log(numMultiplication);
+//     let numAddition = (numMultiplication + 10);
+//     // console.log(numAddition);
+//     let numDivision = (numAddition / 2);
+//     // console.log(numDivision);
+//     let numSubtraction = (numDivision - 5);
+//     let finalResult = numSubtraction;
+//     // console.log(finalResult);
+//     // return finalResult;
+// }
+// mindGame();
+    
+
+// function mindGame(inputNumber){
+//     let numMultiplication = (inputNumber * 3);
+//     // console.log(numMultiplication);
+//     let numAddition = (numMultiplication + 10);
+//     // console.log(numAddition);
+//     let numDivision = (numAddition / 2);
+//     // console.log(numDivision);
+//     let numSubtraction = (numDivision - 5);
+//     let finalResult = numSubtraction;
+//     // return finalResult;
+// }
+// let finalOutcome = mindGame(18);
+// // console.log(finalOutcome);
+    
+
+
+
+
+    
+
+
+
+// -------------------------SECOND PROBLEM-------------------------------
+
+
+// Function Details------
+//    Function Name :      evenOdd
+// (1) Take a string value as input
+// (2) If the number of 'Character Value' of the string is even then return even
+// (3) If the number of 'Character Value' of the string is odd then return odd
+
+
+
+
+
+// the main focus of writing the function is to define the character numbers of a particular string is either even or odd. I follow the instructions of the instructor.
+
+
+
+// function evenOdd(){
+//     let stringValue = 'tahnuma';
+//     let stringChar = stringValue.length;
+//     let remainder = stringChar % 2;
+// if(remainder === 0){
+//     return "even";
+    
+// }
+// else{
+//     return "odd";
+    
+// }
+// }
+// let finalOutput = evenOdd();
+// console.log(finalOutput);
+
+
+
+
+ 
+
+// function evenOdd(stringValue){
+//     stringChar = stringValue.length;
+//     let remainder = stringChar % 2;
+//     if(remainder === 0){
+//         return "even";
+//     }
+//     else{
+//         return "odd";
+//     }
+//     }
+//     let isEven = evenOdd('tahnuma');
+//     console.log(isEven);
+
+
+
+
+
+
+//-----------------------------THIRD PROBLEM-----------------------------------------
+  
+
+// Function Details
+
+// (1) Function Name:      isLGSeven
+// (2) subtract 7 from the above number
+// (3) if the subtraction result is equal or greater than 7 then return the value as ' 2 times of the taken input value'.
+// (4) if the subtraction result is less than 7 then return value as same as you get
+
+
+
+// the main purpose of writing the function is to show less than or greater than of a result using operations. By following the instructions.
+
+
+
+
+// function isLGSeven(input){
+//     let differValue = (input - 7);
+//     if(differValue >= 7){
+//         return input*2;
+//     }
+//     else{
+//         return differValue;
+//     }
+// }
+// let result = isLGSeven(18);
+// console.log(result);
